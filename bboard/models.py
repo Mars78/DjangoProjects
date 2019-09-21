19fe204d1ac4c1fdcfe9875080663a3d8246cf46
Mars78 <sevlar78@gmail.com>
2019-09-21 22:02:22 +0300

    admin

--- bboard/models.py
+++ bboard/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(blank=True, null=True)
-    price = models.FloatField(blank=True, null=True)
-    date = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(blank=True, null=True, verbose_name='Описание')
+    price = models.FloatField(blank=True, null=True, verbose_name='Цена')
+    date = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-date']
